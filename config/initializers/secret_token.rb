0a6a950163cf00a629fd558a5faed158647042a5
Jake Bladt <jake@jakebladt.com>
2014-05-17 15:29:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CherryIvy::Application.config.secret_key_base = '93f237c9d6c1579f185edd94dc97a93c7ced4d101d3f40183bb00e5cd5820761a828f72cb954bbb97b606ddeb769981baf4f8e3e6cf5550bf35276426b6cd293'
