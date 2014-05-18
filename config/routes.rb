52421ff1d469b050ae375a92b1b5f9e03b517acb
Frances Burch <fburchdev@gmail.com>
2014-05-17 22:50:57 -0400

    calendars controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CherryIvy::Application.routes.draw do
+  get "calendars/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
