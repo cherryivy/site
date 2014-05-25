059d06e32a7a7c821711c74406b923950dc4ff8c
Jake Bladt <jake@jakebladt.com>
2014-05-25 18:41:27 -0400

    initial user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: "Jake", email: "jake@jakebladt.com", login: "jake", role: "superuser ") 
+  end
+  
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:role) }
+  it { should respond_to(:login) }
 end
