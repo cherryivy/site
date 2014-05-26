b9abfd40c7c2f1366024e8b8d22584283bdad486
Jake Bladt <jake@jakebladt.com>
2014-05-26 01:06:33 -0400

    unique login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526050135) do
+ActiveRecord::Schema.define(version: 20140526050514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,6 @@ ActiveRecord::Schema.define(version: 20140526050135) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["login"], name: "index_users_on_login", using: :btree
+  add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
 
 end
