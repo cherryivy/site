bdb020fc761da5be461428db2586da7a9b67da18
Jake Bladt <jake@jakebladt.com>
2014-05-26 01:00:19 -0400

    validate uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true
-  validates :email, presence: true
-  validates :login, presence: true 
+  validates :name,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  validates :login, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
