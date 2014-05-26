67c56933bc64cf4f2258213e864b57eeb8e1282b
Jake Bladt <jake@jakebladt.com>
2014-05-26 00:10:16 -0400

    validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Jake", email: "jake@jakebladt.com", login: "jake", role: "superuser ") 
+    @user = User.new(name: "Jake", email: "jake@jakebladt.com", login: "jake") 
   end
   
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  it { should respond_to(:role) }
+  it { should respond_to(:type) }
   it { should respond_to(:login) }
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
 end
