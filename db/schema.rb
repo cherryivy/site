1661ad46cd2742b641f75950416c80af00d09cd7
Jake Bladt <jake@jakebladt.com>
2014-05-26 01:02:54 -0400

    unique email migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526035341) do
+ActiveRecord::Schema.define(version: 20140526050135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140526035341) do
     t.string   "type"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["login"], name: "index_users_on_login", using: :btree
 
 end
