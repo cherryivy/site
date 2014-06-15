52c0b84806c5caac480f93df7115befc2f9df4af
Jake Bladt <jake@jakebladt.com>
2014-06-15 11:56:24 -0400

    sessions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,10 @@
 CherryIvy::Application.routes.draw do
+  resources :charities,      except: :destroy
+  resources :philantropists, except: :destroy
+  resources :events
+
+  root to: 'events/index'
   match '/signup', to: 'philanthropists#new', via: 'get'
-  get "charities/new"
-  get "charities/update"
-  get "charities/view"
-  get "philanthropists/create"
-  get "philanthropists/edit"
-  get "philanthropists/update"
-  get "philanthropists/view"
-  root "calendars#index"
-  get "reports/show"
-  get "events/index"
-  get "events/create"
-  get "events/show"
-  get "events/edit"
-  get "events/deactivate"
-  get "charities/index"
-  get "charities/create"
-  get "charities/edit"
-  get "charities/show"
-  get "charities/deactivate"
-  get "calendars/index"
+  match '/login', to:  'sessions#new', via: 'get'
+  match '/logout', to: 'sessions#destroy', via: 'get'
 end
