bf2d6719c698372768e88026f7b8fffc4277d229
Jake Bladt <jake@jakebladt.com>
2014-06-15 18:26:00 -0400

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 CherryIvy::Application.routes.draw do
-  resources :charities,      except: :destroy
-  resources :philantropists, except: :destroy
+  resources :charities,       except: :destroy
+  resources :philanthropists, except: :destroy
   resources :events
+  resource  :session
 
-  root to: 'events/index'
-  match '/signup', to: 'philanthropists#new', via: 'get'
-  match '/login', to:  'sessions#new', via: 'get'
-  match '/logout', to: 'sessions#destroy', via: 'get'
+  root to: 'events#index'
+  match '/signup', to: 'philanthropists#new', via: 'get', as: 'signup'
+  match '/login',  to: 'sessions#new',        via: 'get', as: 'login'
+  match '/logout', to: 'sessions#destroy',    via: 'get', as: 'logout'
 end
