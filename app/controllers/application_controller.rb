55201c18ec048736f9d6e5d35ddb67563b94bdef
Jake Bladt <jake@jakebladt.com>
2014-07-04 15:57:23 -0400

    role-limit filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,17 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_url, alert: 'Please sign in.' unless current_user
   end
 
+  def limit_to(roles)
+    valid_roles = []
+    if roles.is_a? Symbol
+      valid_roles << roles.camelize.constantize
+    else
+      roles.each { |r| valid_roles << r.camelize.constantize }
+    end
+
+    allowed = false
+    valid_roles.each { |v| allowed = true if current_user.is_a? v }
+    allowed
+  end
+
 end
