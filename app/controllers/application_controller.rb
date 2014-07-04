d1a81ed54e29add732960f14c61d007ee7801855
Jake Bladt <jake@jakebladt.com>
2014-07-03 21:56:22 -0400

    authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end 
+  end
+
+  def authenticate
+    redirect_to new_session_url, alert: 'Please sign in.' unless current_user
+  end
 
 end
