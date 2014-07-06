f1bb2881b3ce0378da54a2984328ec2a1399fb9e
Jake Bladt <jake@jakebladt.com>
2014-07-06 13:43:56 -0400

    login by login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
    end
  
    def create
-     user = User.where(email: params[:email]).first
+     user = User.where(login: params[:login]).first
  
      if user && user.authenticate(params[:password])
        session[:user_id] = user.id
