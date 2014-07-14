43ec9fa1bf8dfe07e96ec7af06012499d2fcc3b9
Jake Bladt <jake@jakebladt.com>
2014-07-14 01:21:57 -0400

    non-persistent connection for uploads

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,8 @@ CherryIvy::Application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :persistent => false 
     }
   }
 
